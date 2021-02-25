bae0f87dca6bea90be80fefcf4850478232121dd
Pair 30 <64235167+Erick-Hoogstrate@users.noreply.github.com>
2021-02-25 20:08:15 +0100

    Add files via upload

--- Combustion_engine.m
+++ Combustion_engine.m
@@ -35,19 +35,44 @@ hair_a= Yair*hia';                                                          % Ma
 sair_a= Yair*sia';                                                          % same but this thermal part of entropy of air for range of T
 
 %% ---------------Code------------
-%% Fuel composition
-Yfuel = [1 0 0 0 0];                                                        % Only fuel
+% %% Fuel composition
+% Yfuel = [1 0 0 0 0];                                                        % Only fuel
+% 
+% density_gasoline = 1.319;                                                    %kg/liter
+% mfurate = 1.3*density_gasoline*1/3600*kg/s;  
+% r = 8.5;
+% 
+% x= SpS(1).Elcomp(3);                                                        %The amount of moles of carbon in the reactants, equal to the amount of moles of C02 in the products
+% y= SpS(1).Elcomp(2);                                                        %the amount of moles of hydrogen in the reactants                                             
+% O2_consumed = (x + (y/4))                                                   %The amount of moles of 02 in the air consumed by the ideal stochiometric combustion proccess,from eq 3.29 in Turns
+% MFuel = SpS(1).Mass;                                                        %defining the molar mass of the fuel in a variable from the SpS struct
+% AF_stoic = 4.76 * O2_consumed * ( MAir / MFuel);                            %dirrectly from equation 3.30 in the book
+% AF = AF_stoic;
 
-density_gasoline = 1.319;                                                    %kg/liter
-mfurate = 1.3*density_gasoline*1/3600*kg/s;  
-r = 8.5;
+%% Stoichiometric combustion
+% CxHyOz + (x + y/4-z/2)(O2) --> xCO2 + (y/2)H2O
+x_gas = SpS(1).Elcomp(3);
+y_gas = SpS(1).Elcomp(2);
+z_gas = 0;
 
-x= SpS(1).Elcomp(3);                                                        %The amount of moles of carbon in the reactants, equal to the amount of moles of C02 in the products
-y= SpS(1).Elcomp(2);                                                        %the amount of moles of hydrogen in the reactants                                             
-O2_consumed = (x + (y/4))                                                   %The amount of moles of 02 in the air consumed by the ideal stochiometric combustion proccess,from eq 3.29 in Turns
-MFuel = SpS(1).Mass;                                                        %defining the molar mass of the fuel in a variable from the SpS struct
-AF_stoic = 4.76 * O2_consumed * ( MAir / MFuel);                            %dirrectly from equation 3.30 in the book
-AF = AF_stoic;
+x_eth = 2;
+y_eth = 6;
+z_eth = 1;
+%% Molair masses
+C = Sp(myfind({Sp.Name},{'C'})).Mass * 1000;      % Carbon    [g/mol]
+H = Sp(myfind({Sp.Name},{'H'})).Mass * 1000;      % Hydrogen  [g/mol]
+O = Sp(myfind({Sp.Name},{'O'})).Mass * 1000;      % Oxygen    [g/mol]
+N = Sp(myfind({Sp.Name},{'N'})).Mass * 1000;      % Nitrogen  [g/mol]
+%% AF Gasoline
+M_gas = C*x_gas + H*y_gas + O*z_gas;                        % [g]
+M_air_gas = (x_gas+(y_gas/4)-(z_gas/2))*(O*2 + 3.76*N*2);   % [g]
+MO2_gas = (x_gas+(y_gas/4)-(z_gas/2))*O*2;                  % [g]
+AF_st_gas = M_air_gas/M_gas
+
+M_eth = C*x_eth + H*y_eth + O*z_eth;                        % [g]
+M_air_eth = (x_eth+(y_eth/4)-(z_eth/2))*(O*2 + 3.76*N*2);   % [g]
+MO2_eth = (x_eth+(y_eth/4)-(z_eth/2))*O*2;                  % [g]
+AF_st_eth = M_air_eth/M_eth
 %%
 %state variables
 p1 = Pamb;
@@ -61,22 +86,25 @@ gamma = 1.4;
 %engine geometric parameters
 bore = 0.068;
 stroke = 0.054;
+radius = bore/2;
 rod = 0.091313;
-r = 8.5;
+r = 8.5;                                                                    %compression ratio
 RPM = 3000;
 
 %calculating swept volume and the clearance volume
 v_d = (pi/4)*bore^2*stroke;
 v_c = v_d/(r-1); 
 
-%starting point
-p0 = p1;
-v0 = (v_d + v_c)/r
 
 %point 1
 v1 = v_d + v_c;
 v2 = v_c;
 g = 1.4;    %gamma
+
+%starting point / point 0
+p0 = p1;
+v0 = (v_d + v_c)/r
+T0 =(p0*v0*T1/(p1*v1));
 
 %calculation of state variables at state point 2
 %p2*v2^g=p1*v1^g
@@ -111,14 +139,14 @@ plot(v3,p3,'*','color','r')
 plot(v4,p4,'*','color','r')
 P0=plot([v0 v1],[p0 p1],'-','color','blue');%isobaric expansion
 P1=plot(v_compression,p_compression,'color','m');%isentropic compression
-P2=plot([v2 v3],[p2 p3],'color','b');%constant volume heat addition
+P2=plot([v2 v3],[p2 p3],'color','b');%isochoric heat addition
 P3=plot(v_expansion,p_expansion,'color','k');%isentropic expansion
-P4=plot([v4 v1],[p4,p1],'color','g');%constant volume heat rejection
+P4=plot([v4 v1],[p4,p1],'color','g');%isochoric heat rejection
 P5=plot([v1 v0],[p1 p0],'--','color','yellow');%isobaric compresion
 xlabel('volume ')
 ylabel('pressure')
 title({'PV Diagram';'Otto cycle'})
-legend([P0 P1 P2 P3 P4 P5],{'isobaric expansion','isentropic compression','constant volume heat addition','isentropic expansion','constant volume heat rejection', 'isobaric compression'})
+legend([P0 P1 P2 P3 P4 P5],{'isobaric expansion','isentropic compression','isochoric heat addition','isentropic expansion','isochoric heat rejection', 'isobaric compression'})
 
 %thermal efficiency
 thermalefficiency=(1-1/r^(g-1))    
@@ -137,50 +165,129 @@ work = area34-area12%Work per cycle
 Tw = 323;      % Estimation of the wall temperature of the engine (50 degrees celcius)
 C0 = 120;    % between 110-130
 % P =       % dependent on crank angle function
-% A =       % dependent on crank angle function
 V_mp = 2*stroke*(RPM/60);
+P_mot = 5*Pamb;
 
 % h_g = C0*bore^-0.2*P^0.8*((C1*V_mp)+(C2*(V_d*T1)/(p1*v1)*(P-P_mot)))^0.8*T^-0.53
 
 % step 0-1 (isobaric expansion)
 C1 = 6.18;
 C2 = 0;
-P = p0;
-T = T1;
+P_begin = p0;
+P_end = p1;
+T_begin = T0;
+T_end = T1;
+V_begin = v0;
+V_end = v1;
+% A_01 = 2*v1/radius  
 
-h_g_01 = C0*bore^-0.2*P^0.8*((C1*V_mp))^0.8*T^-0.53
-
-% dQdt = -h_g_01*A*(T-Tw)
+for T=T_begin:T_end
+    for P=P_begin:P_end
+        h_g_01 = C0*bore^-0.2*P^0.8*((C1*V_mp))^0.8*T^-0.53;
+    end
+    for v=V_begin:V_end
+        A_01 = 2*v/radius;
+    end
+    dQdt = -h_g_01*A_01*(T-Tw);
+end
 
 
 % step 1-2 (isentropic compression)
 C1 = 2.28;
 C2 = 0;
 P = p_compression;
-T = T2;
+% P_begin = p0;
+% P_end = p1;
+T_begin = T1;
+T_end = T2;
+A_12 = 2*v_compression/radius;  
 % T = (p_compression.*v_compression*T1/(p1*v1);     % Unclear what T should be used and whether this should be a constant value or a function
 
-h_g_12 = C0*bore^-0.2*P.^0.8.*((C1*V_mp))^0.8*T^-0.53
+for T=T_begin:T_end
+%     for P=P_begin:P_end
+    h_g_12 = C0*bore^-0.2*P.^0.8.*((C1*V_mp))^0.8*T^-0.53;
+%     end
+    dQdt = -h_g_12.*A_12.*(T-Tw);
+end
 
 
-% dQdt = -h_g*A*(T3-Tw)
+% step 2-3 (isochoric heat addition)
+C1 = 2.28;
+C2 = 3.24*10^-3;
+P_begin = p2;
+P_end = p3;
+T_begin = T2;
+T_end = T3;
+A_23 = 2*v2/radius;  
+
+for T=T_begin:T_end
+    for P=P_begin:P_end
+        h_g_23 = C0*bore^-0.2*P^0.8*((C1*V_mp)+(C2*(v_d*T1)/(p1*v1)*(P-P_mot)))^0.8*T^-0.53;
+    end
+    dQdt = -h_g_23*A_23*(T-Tw);
+end
+
+
+% step 3-4 (isentropic expansion)
+C1 = 2.28;
+C2 = 3.24*10^-3;
+P = p_expansion;
+% P_begin = p3;
+% P_end = p4;
+T_begin = T3;
+T_end = T4;
+A_34 = 2*v_expansion/radius;  
+
+for T=T_begin:T_end
+%     for P=P_begin:P_end
+    h_g_34 = C0*bore^-0.2*P^0.8*((C1*V_mp)+(C2*(v_d*T1)/(p1*v1)*(P-P_mot)))^0.8*T^-0.53;
+%     end
+    dQdt = -h_g_01*A_34*(T-Tw);
+end
+
+
+% Unknown which coefficients need to be used
+% % step 4-1 (isochoric heat rejection)
+% C1 = 6.18;
+% C2 = 0;
+% P = p0;
+% T = T1;
+% A_41 = 2*v1/radius;  
+% 
+% h_g_01 = C0*bore^-0.2*P^0.8*((C1*V_mp))^0.8*T^-0.53;
+% dQdt = -h_g_01*A_41*(T-Tw);
+
+
+% step 1-0 (isobaric compression)
+C1 = 6.18;
+C2 = 0;
+P_begin = p1;
+P_end = p0;
+T_begin = T1;
+T_end = T0;
+V_begin = v1;
+V_end = v0;
+% A_10 = 2*v/radius;  
+
+for T=T_begin:T_end
+    for P=P_begin:P_end
+        h_g_10 = C0*bore^-0.2*P^0.8*((C1*V_mp))^0.8*T^-0.53;
+    end
+    for v=V_begin:V_end
+        A_10 = 2*v/radius; 
+    end
+    dQdt = -h_g_10*A_10*(T-Tw);
+end
 
 
 
 return
-cv=cp/gamma_cv%Use NasaUseExample.m to retrieve cp and gamma. From this you can get cv. Gamma specific to substance.
+cv = cp/gamma_cv%Use NasaUseExample.m to retrieve cp and gamma. From this you can get cv. Gamma specific to substance.
 T4=(p4/p1)*T1%Calculated with ideal gas law
 Qc = cv*(MAir+MFuel)*(T4-T1)
 
-
-
-%%
 %efficiency
-
 efficiency = work/(work+Qc)
-
-
-
 
 
 
